48f35f4db14b48ce87814193053c80bb0a63d00d
Juan Granados <jgranados@est.ups.edu.ec>
2021-12-30 15:51:43 -0500

    un cambio pequeño

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -5,25 +5,25 @@ const router = express.Router();
 router.get("/", (req, res) => {
   Orders.find()
     .exec()
-    .then((x) => res.status(200).send(x));
+    .then((x) => res.status(200).send(x))
 });
 router.get("/:id", (req, res) => {
   Orders.findById(req.params.id)
     .exec()
-    .then((x) => res.status(200).send(x));
+    .then((x) => res.status(200).send(x))
 });
 router.post("/", (req, res) => {
-  Orders.create(req.body).then((x) => res.status(201).send(x));
+  Orders.create(req.body).then((x) => res.status(201).send(x))
 });
 router.put("/:id", (req, res) => {
   Orders.findOneAndUpdate(req.params.id, req.body).then((x) =>
     res.sendStatus(204)
-  );
+  )
 });
 router.delete("/:id", (req, res) => {
   Orders.findOneAndDelete(req.params.id)
     .exec()
-    .then(() => res.sendStatus(204));
+    .then(() => res.sendStatus(204))
 });
 
 module.exports = router;
